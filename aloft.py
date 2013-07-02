038a2ff796c60903e31c136369ba43fc9e5b7765
Mayur <msp48@compute-7-4.local>
2013-07-02 11:18:24 -0400

    Enabling network parameters for now..

--- aloft.py
+++ aloft.py
@@ -908,7 +908,7 @@ if __name__ == "__main__":
     
                 ##calculate distance to recessive genes
                 gene_name = outdata["gene"]
-                if False and gene_name in ppi: #since this code takes too long, going to not evaluate it
+                if gene_name in ppi: #since this code takes too long, going to not evaluate it
                     dominantdist, numberOfDominantNeighbors = parsePPI(ppi, gene_name, dgenes)
                     outdata["shortest path to dominant gene"] = 'N/A' if dominantdist == 0 else str(dominantdist)
                     outdata["dominant neighbors"] = str(numberOfDominantNeighbors)
