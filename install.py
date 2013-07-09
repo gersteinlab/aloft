f615c8f64b92bebaed7906f33b98e2d4b457db7b
Mayur <msp48@login-0-1.local>
2013-07-09 16:41:21 -0400

    quickly packaging networkx for now.

--- install.py
+++ install.py
@@ -1,9 +1,21 @@
 #!/usr/bin/env python
-import subprocess
-import os
+import subprocess, os, shutil
 from vat.install_vat import buildVat
 
 subprocess.call(['cc', 'gerprate.c', '-Wall', '-o', 'gerprate'])
 
-os.chdir('vat')
-buildVat()
+networkModule = 'networkx'
+networkDirectory = networkModule+'-1.7'
+
+if os.path.exists(networkDirectory):
+	shutil.rmtree(networkDirectory)
+subprocess.call(['tar', '-zxvf', networkDirectory+".tar.gz"])
+
+if os.path.exists(networkModule):
+	shutil.rmtree(networkModule)
+shutil.move(os.path.join(networkDirectory, networkModule), networkModule)
+
+shutil.rmtree(networkDirectory)
+
+#os.chdir('vat')
+#buildVat()
